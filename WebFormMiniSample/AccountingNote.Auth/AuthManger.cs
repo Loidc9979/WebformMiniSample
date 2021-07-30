9a5e7db8ea83957fe13678ee1b21044da1aae917
Loidc9979 <87354885+Loidc9979@users.noreply.github.com>
2021-07-30 14:51:04 +0900

    【重構】加入判斷是否登入的程式

---
+++ WebFormMiniSample/AccountingNote.Auth/AuthManger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AccountingNote.Auth
+{
+    public class AuthManger
+    {
+        /// <summary>
+        /// 負責處理登入的元件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsLogined()
+        {
+            /// <summary>
+            /// 檢查目前是否登入
+            /// </summary>
+            /// <returns></returns>
+            if (HttpContext.Current.Session["UserLoginInfo"] == null)
+                return false;
+            else
+                return true;
+        }
+    }
+}
